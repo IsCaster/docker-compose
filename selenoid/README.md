2a026aa13a7aec2864de22ae6752079109c2b00c
Caster Zeng <zxc262403@gmail.com>
2019-04-10 11:44:24 +0800

    feat: add docker-compose for seleniud

---
+++ selenoid/README.md
@@ -0,0 +1,18 @@
+# docker-compose for selenoid
+
+## install
+
+```bash
+./pull_images.sh
+docker-compose up -d
+```
+
+## selenium visit address
+
+### vnc
+
+http://localhost:8090
+
+### selenium grid
+
+http://localhost:4444/wd/hub
---
+++ selenoid/pull_images.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+docker pull selenoid/vnc:chrome_65.0
+docker pull selenoid/vnc:chrome_66.0
+docker pull selenoid/vnc:firefox_59.0
+docker pull selenoid/vnc:firefox_60.0
+docker pull aerokube/selenoid
+docker pull aerokube/selenoid:1.7.2
+docker pull aerokube/selenoid-ui
